70389101faaef290579d1f49f8d4fbce9ac03226
lipeng <lipeng_ly@163.com>
2017-03-12 09:35:37 +0800

    添加readme

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,37 @@ $(function () {
         // $(".list li a[href^='index']").addClass("on");
     }
 })
+
+$(function(){
+    $.ajax({
+        url:'/test',
+        success:function (data) {
+            console.log(arguments)
+        },
+        error:function (jqXHR,textStatus,err) {
+            //jqXHR jquery增强的xhr
+            //textStatus，请求完成状态
+            //err错误信息
+        },
+        complete:function (jqXHR,textStatus) {
+            
+        },
+        statusCode:{ //成员是对象
+            '403':function (jqXHR,textStatus,err) {
+                
+            },
+            '400':function () {
+                
+            }
+            
+        }
+        
+
+
+    });
+
+
+
+
+})
+
